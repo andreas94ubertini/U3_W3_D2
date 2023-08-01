65a3fed36279f1542ccfe6168f9efc2f22a8b142
andreas ubertini <ubertiniandreas@gmail.com>
2023-08-01 16:50:57 +0200

    completed

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
 
   form!:FormGroup;
   gender:string[] = ["uomo", "donna", "altro",]
